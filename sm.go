ad613a516567bcef1b61f1e796b07be1455c97b5
apetsko <apetskom@gmail.com>
2024-02-08 00:05:55 +0600

    sm changes

--- sm.go
+++ sm.go
@@ -3,7 +3,9 @@ package imgocache
 import "sync"
 
 // SM type is the same sync.Map type, which is safe to conrurrent read-write operations
-type SM sync.Map
+type SM struct {
+	sync.Map
+}
 
 // NewSM creates new SM
 func NewSM() *SM {
@@ -12,17 +14,17 @@ func NewSM() *SM {
 
 // Set method sets the value v for the key k in the cache
 func (s *SM) Set(k string, v interface{}) {
-	s.Set(k, v)
+	s.Store(k, v)
 }
 
 // Get method retrieves the value v for the key k from the cache
 // It returns v and a boolean indicating whether the value exists or not
 func (s *SM) Get(k string) (v interface{}, exist bool) {
-	v, exist = s.Get(k)
+	v, exist = s.Load(k)
 	return
 }
 
 // Delete method deletes the value from the cache for the given key
 func (s *SM) Delete(k string) {
-	s.Delete(k)
+	s.Store(k, nil)
 }
